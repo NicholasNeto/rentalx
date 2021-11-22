620512f61741cbd9662fb97cff1c225b00467a01
NicholasNeto <nicholasneto@outlook.com>
2021-11-21 22:10:23 -0300

    created route get categories for get all result of categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,20 @@
 import { Router } from 'express'
 
-import { Category } from '../model/Category';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
-const categoriesRoutes = Router();
-
-const categories: Category[] = []
-
+const categoriesRoutes = Router()
+const categoriesRepository = new CategoriesRepository()
 
 categoriesRoutes.post('/', (request, response) => {
     const { name, description } = request.body
 
-    const category =  new Category()
+    categoriesRepository.create({ name, description })
+    return response.status(201).send()
+})
 
-    Object.assign(category, {
-        name,
-        description,
-        created_at: new Date()
-    })
-
-    categories.push(category)
-    return response.status(201).json({category})
+categoriesRoutes.get('/', (request, response) => {    
+    const categories = categoriesRepository.list()
+    return response.status(201).json(categories)
 })
 
 export { categoriesRoutes }
