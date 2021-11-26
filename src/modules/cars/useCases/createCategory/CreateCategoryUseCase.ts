8d34fcf3b41f0caf15297b49f7fc6bcc0d16ea58
NicholasNeto <nicholasneto@outlook.com>
2021-11-26 06:49:13 -0300

    created folder useCase and controller for category

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -0,0 +1,33 @@
+
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
+
+class CreateCategoryUseCase {
+
+    private categoriesRepository: ICategoriesRepository;
+
+    constructor(categoriesRepository: ICategoriesRepository) {
+        this.categoriesRepository = categoriesRepository
+    }
+
+    execute({ name, description }: IRequest): void {
+
+        const categoryAlreadyExists = this.categoriesRepository.findByName(name)
+
+        if (categoryAlreadyExists) {
+            throw new Error(`Category ${name} already exists`)
+        }
+
+        this.categoriesRepository.create({ name, description })
+
+    }
+
+}
+
+
+export { CreateCategoryUseCase }
