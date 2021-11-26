bcd54dbb0611c44d3072498241277af882a00448
NicholasNeto <nicholasneto@outlook.com>
2021-11-25 22:35:19 -0300

    type without date

--- src/modules/cars/repositories/ISpecificationsRepository.ts
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -9,6 +9,7 @@ interface ICreateSpecificationDTO {
 interface ISpecificationRepository {
   create({ name, description }: ICreateSpecificationDTO): void;
   findByName(name: string): Specification;
+  list(): Specification[]
 }
 
 export { ISpecificationRepository, ICreateSpecificationDTO }
