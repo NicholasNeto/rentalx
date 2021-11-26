4544ea890e2109aa4a63333920008a8f6c85e92c
NicholasNeto <nicholasneto@outlook.com>
2021-11-26 07:25:27 -0300

    used new controller ListCategoriesController

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,17 +2,17 @@ import { Router } from 'express'
 
 import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository'
 import { createCategoryController } from '../modules/cars/useCases/createCategory'
+import { listCategoriesController } from '../modules/cars/useCases/listCategory'
 
 const categoriesRoutes = Router()
 const categoriesRepository = new CategoriesRepository()
 
 categoriesRoutes.post('/', (request, response) => {
-    createCategoryController.handle(request, response)
+    return createCategoryController.handle(request, response)
 })
 
 categoriesRoutes.get('/', (request, response) => {
-    const categories = categoriesRepository.list()
-    return response.status(201).json(categories)
+    return listCategoriesController.handle(request, response)
 })
 
 export { categoriesRoutes }
