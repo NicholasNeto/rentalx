260615f35bdc0ea578d2099fc13a3c7b98e411d3
NicholasNeto <nicholasneto@outlook.com>
2022-02-03 20:46:47 -0300

    change import and implement new Entiti of Category

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,5 +1,5 @@
 
-import { Specification } from "../../model/Specification";
+import { Specification } from "../../entities/Specification";
 import { ICreateSpecificationDTO, ISpecificationRepository } from "../ISpecificationsRepository";
 
 class SpecificationsRepository implements ISpecificationRepository {
