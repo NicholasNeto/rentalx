24d2bf3a3ba90306e8155b13ee9a67e8abc88d9f
NicholasNeto <nicholasneto@outlook.com>
2022-02-07 22:46:39 -0300

    fix router and created new container for listCategories and especification

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,30 +1,22 @@
 import { Router } from 'express'
 import multer from 'multer'
 
-import { importCategoryController } from '../modules/cars/useCases/importCategory'
-import { listCategoriesController } from '../modules/cars/useCases/listCategory'
+import { ImportCategoryController } from '../modules/cars/useCases/importCategory/ImportCategoryController'
+import { ListCategoriesController } from '../modules/cars/useCases/listCategory/ListCategoriesController'
 import { CreateCategoryController } from '../modules/cars/useCases/createCategory/CreateCategoryController'
 
 
 const categoriesRoutes = Router()
-
-
-const createCategoryController = new CreateCategoryController()
-
-categoriesRoutes.post('/', createCategoryController.handle)
-
-categoriesRoutes.get('/', (request, response) => {
-    return listCategoriesController.handle(request, response)
-})
-
-
 const upload = multer({
     dest: './tmp',
 })
 
-categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
-    return importCategoryController.handle(request, response)
-});
+const createCategoryController = new CreateCategoryController()
+const importCategoryController = new ImportCategoryController()
+const listCategoriesController = new  ListCategoriesController()
 
+categoriesRoutes.post('/', createCategoryController.handle)
+categoriesRoutes.get('/', listCategoriesController.handle)
+categoriesRoutes.post('/import', upload.single('file'), importCategoryController.handle);
 
 export { categoriesRoutes }
--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,18 +1,12 @@
 import { Router } from 'express'
-import { createSpecificationController } from '../modules/cars/useCases/createSpecification'
+import { CreateSpecificationConroller } from '../modules/cars/useCases/createSpecification/CreateSpecificationConroller'
 import { listCategoriesController } from '../modules/cars/useCases/listCategory'
 
 const specificationsRoutes = Router()
 
+const createSpecificationConroller = new CreateSpecificationConroller()
 
-specificationsRoutes.post('/', (request, response) => {
-
-    return createSpecificationController.handle(request, response)
-
-
-
-    return response.status(201).send()
-})
+specificationsRoutes.post('/', createSpecificationConroller.handle);
 
 specificationsRoutes.get('/', (request, response) => {
     listCategoriesController.handle
