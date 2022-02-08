75241f9d28606e25a4f55b2eb81d1ad3623efd42
NicholasNeto <nicholasneto@outlook.com>
2022-02-07 22:45:39 -0300

    created inject dependencies of list categories

--- src/modules/cars/useCases/listCategory/ListCategoriesUseCase.ts
+++ src/modules/cars/useCases/listCategory/ListCategoriesUseCase.ts
@@ -1,17 +1,16 @@
+import { inject, injectable } from "tsyringe";
+import { Category } from "../../entities/Category";
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
-
+@injectable()
 class ListCategoriesUseCase {
 
+    constructor(
+        @inject("CategoriesRepository")
+        private categoriesRepository: ICategoriesRepository) { }
 
-    private categoriesRepository: ICategoriesRepository;
-
-    constructor(categoriesRepository: ICategoriesRepository) {
-        this.categoriesRepository = categoriesRepository
-    }
-
-    execute() {
-        const categories = this.categoriesRepository.list()
+    async execute(): Promise<Category[]> {
+        const categories = await this.categoriesRepository.list()
         return categories
     }
 }
