dafd13e58297eb6a12cf5bf62750b08ca08975e6
NicholasNeto <nicholasneto@outlook.com>
2022-02-13 21:20:08 -0300

    created IUserRepository

---
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -0,0 +1,16 @@
+
+
+interface ICreateUserDTO {
+    name: string;
+    username: string;
+    password: string;
+    email: string;
+    driver_license: string;
+}
+
+
+interface IUsersRepository  {
+    create(data: ICreateUserDTO): Promise<void>
+}
+
+export { IUsersRepository }
