5f48c76c58f274ff0da4abf483fb68c402bec16a
NicholasNeto <nicholasneto@outlook.com>
2022-02-19 18:14:43 -0300

    implementation of findById

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated'
 import { CreateSpecificationConroller } from '../modules/cars/useCases/createSpecification/CreateSpecificationConroller'
 import { listCategoriesController } from '../modules/cars/useCases/listCategory'
 
@@ -6,6 +7,7 @@ const specificationsRoutes = Router()
 
 const createSpecificationConroller = new CreateSpecificationConroller()
 
+specificationsRoutes.use(ensureAuthenticated)
 specificationsRoutes.post('/', createSpecificationConroller.handle);
 
 specificationsRoutes.get('/', (request, response) => {
