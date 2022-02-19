accbd2ce8ccf58709c2a10bd61e2cbd363aff290
NicholasNeto <nicholasneto@outlook.com>
2022-02-19 18:25:38 -0300

    used new class AppError

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
+import { AppError } from "../errors/AppError";
 import { UserRepository } from "../modules/accounts/repositories/implementations/UserRepository";
 
 
@@ -7,28 +8,28 @@ interface IPayload {
     sub: string
 }
 
-export async function ensureAuthenticated(request : Request, response: Response, next: NextFunction) {
-   const authHeader =  request.headers.authorization;
+export async function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
+    const authHeader = request.headers.authorization;
 
-   if(!authHeader){
-    throw new Error("Token missing")
-   }
+    if (!authHeader) {
+        throw new AppError("Token missing", 401)
+    }
 
-   const [, token] = authHeader.split(" ")
+    const [, token] = authHeader.split(" ")
 
-   try {
-        const { sub: user_id }  = verify(token, "cb7aad368fbbba2f70dffdae85fe1a2e") as IPayload
+    try {
+        const { sub: user_id } = verify(token, "cb7aad368fbbba2f70dffdae85fe1a2e") as IPayload
         console.log("---> ", user_id)
 
         const userRepository = new UserRepository()
         const user = userRepository.findById(user_id)
 
-        if(!user){
-            throw new Error("USer does not exists!")
+        if (!user) {
+            throw new AppError("USer does not exists!", 401)
         }
-        
+
         next()
-   } catch (error) {
-       throw new Error("Invalid token")
-   }
+    } catch (error) {
+        throw new AppError("Invalid token", 401)
+    }
 }
--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -2,6 +2,7 @@ import { compare } from 'bcrypt'
 import { inject, injectable } from "tsyringe";
 import { sign } from 'jsonwebtoken' 
 import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { AppError } from '../../../../errors/AppError';
 
 interface IRequest {
     email: string;
@@ -29,14 +30,15 @@ class AuthenticateUserUseCase {
         // usuario existe
         const user = await this.userRepository.findByEmail(email)
         if (!user) {
-            throw new Error(`Email or password incorrect!`);
+            throw new AppError(`Email or password incorrect!`);
+            
         }
 
         // Senha está correta
         const passwordMatch = await compare(password, user.password);
 
         if (!passwordMatch) {
-            throw new Error(`Email or password incorrect!`);
+            throw new AppError(`Email or password incorrect!`);
         }
 
         // Gerar jsonwebtoken
