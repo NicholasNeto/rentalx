f0cea7bdf34ec3731726dc319986a21a7cb9e593
NicholasNeto <nicholasneto@outlook.com>
2022-03-20 19:14:32 -0300

    new useCase Car

---
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -0,0 +1,74 @@
+import { Car } from "@modeles/cars/infra/typeorm/entities/Car";
+import { CarsRepositoryInMemory } from "@modeles/cars/repositories/in-memory/CarsRepositoryInMemory"
+import { AppError } from "@shared/errors/AppError";
+import { CreateCarUseCase } from "./CreateCarUseCase"
+
+
+let createCarUseCase: CreateCarUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory
+
+
+describe.only("Create Car", () => {
+
+    beforeEach(() => {
+        carsRepositoryInMemory = new CarsRepositoryInMemory()
+        createCarUseCase = new CreateCarUseCase(carsRepositoryInMemory)
+    })
+
+    it("shold be able to create a new car", async () => {
+        const carCreated = await createCarUseCase.execute(
+            {
+                name: "Name Car",
+                description: "Description Car",
+                daily_rate: 100,
+                license_plate: "ABC-1234",
+                fine_amount: 60,
+                brand: "Brand Car",
+                category_id: "category"
+            }
+        )
+
+        expect(carCreated).toHaveProperty('id')
+
+    })
+
+    it("shold not be able to create a car with exists licenseplate", () => {
+        expect(async () => {
+            await createCarUseCase.execute({
+                name: "Name Car2",
+                description: "Description Car",
+                daily_rate: 100,
+                license_plate: "ABC-12345",
+                fine_amount: 60,
+                brand: "Brand Car",
+                category_id: "category"
+            })
+
+            await createCarUseCase.execute({
+                name: "Name Car2",
+                description: "Description Car",
+                daily_rate: 100,
+                license_plate: "ABC-12345",
+                fine_amount: 60,
+                brand: "Brand Car",
+                category_id: "category"
+            })
+
+        }).rejects.toBeInstanceOf(AppError)
+    })
+
+    it("shold be able to create a car with available true", async () => {
+
+        const car = await createCarUseCase.execute({
+            name: "Name Car2",
+            description: "Description Car",
+            daily_rate: 100,
+            license_plate: "ABC-12345",
+            fine_amount: 60,
+            brand: "Brand Car",
+            category_id: "category"
+        })
+
+        expect(car.available).toEqual(true)
+    })
+})
