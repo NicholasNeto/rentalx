e92acd7eac9661ac6d110c6cb874f7a2c5602719
NicholasNeto <nicholasneto@outlook.com>
2022-03-27 18:34:28 -0300

    create new route available cars

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express'
 import { CreateCarController } from '@modeles/cars/useCases/createCar/CreateCarController'
+import { ListAvailableCarsController } from '@modeles/cars/useCases/listCars/ListCarsController'
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated'
 import { ensureAdmin } from '../middlewares/ensureAdmin'
 
 const carsRoutes = Router()
 
 const createCarController = new CreateCarController()
+const listAvailableCarsController = new ListAvailableCarsController()
+
 carsRoutes.post("/", ensureAuthenticated, ensureAdmin, createCarController.handle)
+carsRoutes.get("/available", listAvailableCarsController.handle)
 
 export { carsRoutes }
