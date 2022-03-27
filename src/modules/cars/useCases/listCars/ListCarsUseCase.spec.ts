e0979ff8289a0ff120459f06796a6c5f2c4400d9
NicholasNeto <nicholasneto@outlook.com>
2022-03-27 11:32:00 -0300

    create test for list of cars

--- src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
+++ src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
@@ -6,41 +6,129 @@ import { ListCarsUseCase } from "./ListCarsUseCase"
 
 
 let listCarsUseCase: ListCarsUseCase;
-let createCarUseCase: CreateCarUseCase;
 let carsRepositoryInMemory: CarsRepositoryInMemory
 
 describe("List Cars", () => {
-    
+
     beforeEach(async () => {
         carsRepositoryInMemory = new CarsRepositoryInMemory()
-        createCarUseCase = new CreateCarUseCase(carsRepositoryInMemory)
         listCarsUseCase = new ListCarsUseCase(carsRepositoryInMemory)
-
-        await createCarUseCase.execute({
-            name: "Name Car1",
-            description: "Description Car1",
-            daily_rate: 100,
-            license_plate: "ABC-123",
-            fine_amount: 60,
-            brand: "Brand Car 1",
-            category_id: "category"
-        })
-
-        await createCarUseCase.execute({
-            name: "Name Car2",
-            description: "Description Car",
-            daily_rate: 100,
-            license_plate: "DEF-123",
-            fine_amount: 50,
-            brand: "Brand Car 2",
-            category_id: "category"
-        })
     })
 
     it("should be able to list all available cars", async () => {
-        const cars = await listCarsUseCase.execute()
 
-        expect(cars).toHaveLength(2)
+        const car = await carsRepositoryInMemory.create(
+            {
+                name: "Name Car1",
+                description: "Description Car1",
+                daily_rate: 100,
+                license_plate: "ABC-123",
+                fine_amount: 60,
+                brand: "Brand Car 1",
+                category_id: "category"
+            }
+        )
+        const cars = await listCarsUseCase.execute({})
+
+        expect(cars).toHaveLength(1)
         expect(cars[0]).toBeInstanceOf(Car)
+        expect(cars[0]).toEqual(car)
+    })
+
+    it("should be able to list all available cars by brand", async () => {
+
+        const car = await carsRepositoryInMemory.create(
+            {
+                name: "Name Car1",
+                description: "Description Car1",
+                daily_rate: 100,
+                license_plate: "ABC-123",
+                fine_amount: 60,
+                brand: "Brand Car 1",
+                category_id: "category"
+            }
+        )
+
+        const car2 = await carsRepositoryInMemory.create(
+            {
+                name: "Name Car1",
+                description: "Description Car1",
+                daily_rate: 100,
+                license_plate: "ABC-123",
+                fine_amount: 60,
+                brand: "Brand Car 2",
+                category_id: "category"
+            }
+        )
+        const cars = await listCarsUseCase.execute({ brand: "Brand Car 1" } )
+
+        expect(cars).toHaveLength(1)
+        expect(cars[0]).toBeInstanceOf(Car)
+        expect(cars[0]).toEqual(car)
+    })
+
+
+    it("should be able to list all available cars by category_id", async () => {
+
+        const car = await carsRepositoryInMemory.create(
+            {
+                name: "Name Car1",
+                description: "Description Car1",
+                daily_rate: 100,
+                license_plate: "ABC-123",
+                fine_amount: 60,
+                brand: "Brand Car 1",
+                category_id: "SUV-xxxx"
+            }
+        )
+
+        const car2 = await carsRepositoryInMemory.create(
+            {
+                name: "Name Car1",
+                description: "Description Car1",
+                daily_rate: 100,
+                license_plate: "ABC-123",
+                fine_amount: 60,
+                brand: "Brand Car 2",
+                category_id: "Sedan"
+            }
+        )
+        const cars = await listCarsUseCase.execute({ category_id: "SUV-xxxx" } )
+
+        expect(cars).toHaveLength(1)
+        expect(cars[0]).toBeInstanceOf(Car)
+        expect(cars[0]).toEqual(car)
+    })
+
+    it("should be able to list all available cars by name", async () => {
+
+        const car = await carsRepositoryInMemory.create(
+            {
+                name: "HR-V",
+                description: "Description Car1",
+                daily_rate: 100,
+                license_plate: "ABC-123",
+                fine_amount: 60,
+                brand: "Brand Car 1",
+                category_id: "SUV-xxxx"
+            }
+        )
+
+        const car2 = await carsRepositoryInMemory.create(
+            {
+                name: "Name Car1",
+                description: "Description Car1",
+                daily_rate: 100,
+                license_plate: "ABC-123",
+                fine_amount: 60,
+                brand: "Brand Car 2",
+                category_id: "Sedan"
+            }
+        )
+        const cars = await listCarsUseCase.execute({ name: "HR-V" } )
+
+        expect(cars).toHaveLength(1)
+        expect(cars[0]).toBeInstanceOf(Car)
+        expect(cars[0]).toEqual(car)
     })
 })
