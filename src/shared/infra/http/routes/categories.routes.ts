de69ff94968311d81226d5d76c8ba68dcdb0b52e
NicholasNeto <nicholasneto@outlook.com>
2022-03-26 23:49:30 -0300

    used new middleware in same routes

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -3,6 +3,8 @@ import { Router } from 'express'
 import { CreateCategoryController } from '@modeles/cars/useCases/createCategory/CreateCategoryController'
 import { ImportCategoryController } from '@modeles/cars/useCases/importCategory/ImportCategoryController'
 import { ListCategoriesController } from '@modeles/cars/useCases/listCategory/ListCategoriesController'
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated'
+import { ensureAdmin } from '../middlewares/ensureAdmin'
 
 
 const categoriesRoutes = Router()
@@ -12,10 +14,19 @@ const upload = multer({
 
 const createCategoryController = new CreateCategoryController()
 const importCategoryController = new ImportCategoryController()
-const listCategoriesController = new  ListCategoriesController()
+const listCategoriesController = new ListCategoriesController()
 
-categoriesRoutes.post('/', createCategoryController.handle)
+categoriesRoutes.post('/',
+    ensureAuthenticated,
+    ensureAdmin,
+    createCategoryController.handle
+)
 categoriesRoutes.get('/', listCategoriesController.handle)
-categoriesRoutes.post('/import', upload.single('file'), importCategoryController.handle);
+categoriesRoutes.post('/import',
+    upload.single('file'),
+    ensureAuthenticated,
+    ensureAdmin,
+    importCategoryController.handle
+);
 
 export { categoriesRoutes }
--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -2,17 +2,21 @@ import { Router } from 'express'
 import { CreateSpecificationConroller } from '@modeles/cars/useCases/createSpecification/CreateSpecificationConroller';
 import { listCategoriesController } from '@modeles/cars/useCases/listCategory';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+import { ensureAdmin } from '../middlewares/ensureAdmin';
 
 const specificationsRoutes = Router()
 
 const createSpecificationConroller = new CreateSpecificationConroller()
 
-specificationsRoutes.use(ensureAuthenticated)
-specificationsRoutes.post('/', createSpecificationConroller.handle);
-
 specificationsRoutes.get('/', (request, response) => {
     listCategoriesController.handle
 })
+
+specificationsRoutes.post('/',
+    ensureAuthenticated,
+    ensureAdmin,
+    createSpecificationConroller.handle
+);
 
 export { specificationsRoutes }
 
