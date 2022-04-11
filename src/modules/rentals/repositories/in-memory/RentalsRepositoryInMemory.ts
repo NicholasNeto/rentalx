1d8a1ed48a393375c1924aae9f06b37baeb2f518
NicholasNeto <nicholasneto@outlook.com>
2022-04-10 21:32:32 -0300

    created created for rental

--- src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
+++ src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
@@ -1,3 +1,4 @@
+import { ICreateRentalDTO } from "@modeles/rentals/dtos/ICreateRental";
 import { Rental } from "@modeles/rentals/infra/typeorm/entities/Rental";
 import { IRentalRepository } from "../IRentalRepository";
 
@@ -7,13 +8,34 @@ class RentalsRepositoryInMemory implements IRentalRepository {
 
     async findOpenRentalByCar(car_id: string): Promise<Rental> {
         return this.rentals.find(
-            rental => rental.car_id === car_id && rental.end_date === null)
+            rental => rental.car_id === car_id && !rental.end_date)
     }
 
     async findOpenRentalByUser(user_id: string): Promise<Rental> {
         return this.rentals.find(
-            rental => rental.user_id === user_id && rental.end_date === null)
+            rental => rental.user_id === user_id && !rental.end_date)
     }
+
+    async create({
+        user_id,
+        car_id,
+        expected_return_date
+    }: ICreateRentalDTO): Promise<Rental> {
+
+        const rental = new Rental()
+
+        Object.assign(rental, {
+            car_id,
+            expected_return_date,
+            user_id,
+            start_date: new Date()
+        })
+
+        this.rentals.push(rental)
+        return rental
+    }
+
+
 }
 
 export { RentalsRepositoryInMemory }
