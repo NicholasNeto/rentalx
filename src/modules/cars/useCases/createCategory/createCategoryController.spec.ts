d52b2a7fbf62d332d323dad4318723254e097b28
NicholasNeto <nicholasneto@outlook.com>
2022-04-15 17:01:24 -0300

    add test for not created a new category if already exists

--- src/modules/cars/useCases/createCategory/createCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/createCategoryController.spec.ts
@@ -23,7 +23,6 @@ describe('Create Category Controller', () => {
             INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
             values('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXXX-XXXX')
         `)
-
     })
 
     afterAll(async () => {
@@ -53,5 +52,31 @@ describe('Create Category Controller', () => {
 
 
         expect(response.status).toBe(201)
+    })
+
+
+
+    it("should not be able to create a new category if already exists", async () => {
+
+        const responseToken = await request(app).post("/sessions").send({
+            email: "admin@rentx.com.br",
+            password: "admin"
+        })
+
+        const { token } = responseToken.body
+
+        const response = await request(app)
+            .post("/categories")
+            .send(
+                {
+                    "name": "Category SuperTest",
+                    "description": "Category SuperTest"
+                }
+            ).set({
+                Authorization: `Bearer ${token}`
+            })
+
+
+        expect(response.status).toBe(401)
     })
 })
