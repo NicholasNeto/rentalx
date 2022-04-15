5690fc7b9be1b4b9d5cf29fb766e3dbb25634e88
NicholasNeto <nicholasneto@outlook.com>
2022-04-15 12:32:24 -0300

    need created new specification list

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,16 +1,17 @@
 import { Router } from 'express'
 import { CreateSpecificationConroller } from '@modeles/cars/useCases/createSpecification/CreateSpecificationConroller';
-import { listCategoriesController } from '@modeles/cars/useCases/listCategory';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 import { ensureAdmin } from '../middlewares/ensureAdmin';
+// import { ListCategoriesController } from '@modeles/cars/useCases/listCategory/ListCategoriesController';
 
 const specificationsRoutes = Router()
 
 const createSpecificationConroller = new CreateSpecificationConroller()
 
-specificationsRoutes.get('/', (request, response) => {
-    listCategoriesController.handle
-})
+// TODO create list specification 
+// specificationsRoutes.get('/', (request, response) => {
+//     ListCategoriesController.handle
+// })
 
 specificationsRoutes.post('/',
     ensureAuthenticated,
